c4d7a030a3b73bedd1b5f039a5bc75e8e4d38ffc
Frank <fha@easv.dk>
2025-11-05 13:20:12 +0100

    new subtract feature

--- src/Calculator.java
+++ src/Calculator.java
@@ -20,4 +20,14 @@ public class Calculator {
         return factor1 * factor2;
     }
 
+    /**
+     * subtract
+     * @param no1
+     * @param no2
+     * @return
+     */
+    public static int subtract(int no1, int no2) {
+        return no1 - no2;
+    }
+
 }
