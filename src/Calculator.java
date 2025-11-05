334ec635b139c3328e0908683c2b32187879e025
Frank <fha@easv.dk>
2025-11-05 13:07:54 +0100

    new calculator class

---
+++ src/Calculator.java
@@ -0,0 +1,13 @@
+public class Calculator {
+
+    /**
+     * Impressive method for adding two numbers
+     * @param sumant1
+     * @param sumant2
+     * @return
+     */
+    public static int sum(int sumant1, int sumant2) {
+        return sumant1 + sumant2;
+    }
+
+}
